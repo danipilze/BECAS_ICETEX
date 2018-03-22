eb18b5f81c5810a69f756587e226cfee75cbb144
Daniel Amariles <daniel.amariles88@gmail.com>
2018-03-22 07:34:30 -0500

    using mechanicalsoup

---
+++ main.py
@@ -0,0 +1,20 @@
+import mechanicalsoup
+from bs4 import BeautifulSoup
+
+
+url="https://www.icetex.gov.co/SIORI_WEB/Convocatorias.aspx?aplicacion=1&vigente=true"
+browser = mechanicalsoup.StatefulBrowser()
+browser.open(url)
+
+browser.select_form("#form1")
+browser["RBLOpcionBuscar"] = "Todas"
+response = browser.submit_selected()
+
+soup = BeautifulSoup(response.text,"html.parser")
+children = list(soup.children)
+
+
+
+opcion2 = soup.find_all(id="");
+
+print(children)
