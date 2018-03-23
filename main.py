afa813c580d10a8a45deb143b1220244986c8a0d
Daniel Amariles <daniel.amariles88@gmail.com>
2018-03-22 21:36:53 -0500

    documentando

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import mechanicalsoup
+import requests
 from bs4 import BeautifulSoup
 
-
+# URL de la página del ICETEX a consultar las becas
 url="https://www.icetex.gov.co/SIORI_WEB/Convocatorias.aspx?aplicacion=1&vigente=true"
-browser = mechanicalsoup.StatefulBrowser()
-browser.open(url)
+browser = mechanicalsoup.StatefulBrowser()  # se define el browser
+browser.open(url)                           # se abre la URL
 
-#seleccionar todas las becas
-browser.select_form("#form1")
-browser["RBLOpcionBuscar"] = "Todas"
-response = browser.submit_selected()
+# Esta página muestra las becas solo cuando se da click en la opción "Todas"
+browser.select_form("#form1")               # seleccionamos el formulario
+browser["RBLOpcionBuscar"] = "Todas"        # seleccionamos que busque con la opción "Todas"
+response = browser.submit_selected()        # enviamos el fomulario y capturamos las respuestas
+# en este punto ya tenemos la tabla #GVConvocatorias con la lista de becas
+# hay que proceder a realizar la extracción de datos teniendo en cuenta la paginación
 
 soup = BeautifulSoup(response.text,"html.parser")
 children = list(soup.children)
 
-
-
-opcion2 = soup.find_all(id="");
 
 print(children)
