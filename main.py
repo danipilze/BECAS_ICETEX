9675f9baf5c04b877a277d3c53829b5bd4624d9a
Daniel Amariles <daniel.amariles88@gmail.com>
2018-03-24 04:18:28 -0500

    retrieving data from scholarship call

--- main.py
+++ main.py
@@ -14,31 +14,28 @@ response = browser.submit_selected()  # enviamos el fomulario y capturamos las r
 # en este punto ya tenemos la tabla #GVConvocatorias con la lista de becas
 # hay que proceder a realizar la extracción de datos teniendo en cuenta la paginación
 
-""""
-# obtener el soup a partir de la respuesta a la acción anterior
-soup = BeautifulSoup(response.text, "html.parser")
-# obtener la tabla que contiene las becas
-table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == "GVConvocatorias")
-# obtener cada fila de la tabla de becas
-rows = table.findAll(lambda tag: tag.name == 'tr')
-
-# para cada fila
-for row in rows:
-    cells = row.findAll('td')  # encontrar las celdas
-    if (len(cells) > 0):
-           print(cells[0])
-"""""
-
-
 # Entrar a cada una de las convocatorias
 #for i in (0,9):
 browser2 = browser
 browser2.select_form("#form1")
+# añadir los parámetros escondidos, usar force=True
+# opción de consulta de convocatoria
 browser2.get_current_form().set("__EVENTTARGET", "GVConvocatorias",  True)
+# identificador de la convocatoria
 browser2.get_current_form().set("__EVENTARGUMENT", "$0",  True) # activar el for y reemplazar el 0 con i
-
-
+# enviar el formulario que consulta la convocatoria específica
 response2 = browser2.submit_selected()
 
-print(response2.text)
+# obtener el soup a partir de la respuesta a la acción anterior
+soup = BeautifulSoup(response2.text, "html.parser")
+# obtener cada fila de la tabla
+rows = soup.findAll("span", { "class" : "label1" })
+
+
+# para cada fila
+for row in rows:
+           print(row)
+
+
+# print(response2.text)
 
